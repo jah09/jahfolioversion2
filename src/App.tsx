c6402782b88c004a01831ae3e3d3f1ba59d8e023
jah09 <jasuhot1@gmail.com>
2024-09-13 23:54:39 +0800

    Setup file structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
- 
 import { Button } from "@/components/ui/button";
 
- 
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl   underline font-roboto">Hello world!</h1>
       <Button>Click me</Button>
     </>
   );
 }
 
-export default App
+export default App;
---
+++ src/pages/Hero.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>
+      <h1>"A software engineer with a primary focus on web development."</h1>
+    </div>
+  );
+}
+
+export default Hero
